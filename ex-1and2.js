af43e320b08363ae24d9be2c14260b97e284b746
Baskitti1 <Baskitti1@gmail.com>
2025-12-01 21:45:08 +0700

    ex-1and2.js

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,19 @@
-//Exercise #1: Variable Creation
-//Start coding here
+// Exercise #1: Variable Creation
+// Start coding here
 
-//Exercise #2: Variable Reassignment
-//Start coding here
+let firstName = "Karter";          // 1.1
+let lastName = "Anderson";         // 1.2
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";   // 1.3
+
+let restaurantName = "Chef's Table by Chef James";   // 1.4
+let totalPrice = 15000;            // 1.5
+let restaurantCount = 1500;        // 1.6
+
+
+
+// Exercise #2: Variable Reassignment
+// Start coding here
+
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";  // ใหม่
+totalPrice = 5000;                 // ใหม่
+restaurantCount = 2500;            // ใหม่
